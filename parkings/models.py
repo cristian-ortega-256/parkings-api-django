4b520c0a8624f0c624c555336563fa5ba4508c68
Cristian <cristian.daniel.ortega@gmail.com>
2018-11-03 13:38:17 -0300

    Adding parking states

--- parkings/models.py
+++ parkings/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Parkings(models.Model):
 	x = models.IntegerField(null=False)
 	y = models.IntegerField(null=False)
+	isOccupied = models.BooleanField(default=False)
 
 	def __str__(self):
-		return "x: {} - y: {}".format(self.x, self.y)
+		return "x: {} - y: {} isOccupied: {}".format(self.x, self.y,self.isOccupied)
