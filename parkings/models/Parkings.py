302f5bffa1c6cf2f8f4c7f755a51d8a04157f627
Cristian <cristian.daniel.ortega@gmail.com>
2018-11-10 14:28:45 -0300

    API services implementation

--- parkings/models/Parkings.py
+++ parkings/models/Parkings.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class Parkings(models.Model):
-	id = models.AutoField(primary_key=True)
-	x = models.IntegerField(null=False)
-	y = models.IntegerField(null=False)
-	isOccupied = models.BooleanField(default=False)
 
-	def __str__(self):
-		return "id: {} ({},{}) isOccupied: {}".format(self.id,self.x, self.y,self.isOccupied)
+class Parkings(models.Model):
+    id = models.AutoField(primary_key=True)
+    tl_x = models.IntegerField(default=0, null=False)
+    tl_y = models.IntegerField(default=0, null=False)
+    br_x = models.IntegerField(default=0, null=False)
+    br_y = models.IntegerField(default=0, null=False)
+    isOccupied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "id: {} ({},{}) isOccupied: {}".format(self.id, self.tl_x, self.tl_y, self.isOccupied)
--- parkings/serializers.py
+++ parkings/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models.Parkings import Parkings
 from .models.Configuration import Configuration
 
+
 class ParkingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parkings
-        fields = ("id","x", "y","isOccupied")
+        fields = ("id", "tl_x", "tl_y", "br_x", "br_y", "isOccupied")
+
 
 class ConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Configuration
-        fields = ("key","value")
+        fields = ("key", "value")
